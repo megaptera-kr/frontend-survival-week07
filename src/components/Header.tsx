9d2535b88e9d45cf8e9248abc88e58bb55920d9c
shiny1912 <shiny1912@naver.com>
2023-04-23 21:32:42 +0900

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><Link to="/restaurants">식당 목록</Link></li>
-                    <li><Link to="/cart">장바구니</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><Link to="/restaurants">식당 목록</Link></li>
+          <li><Link to="/cart">장바구니</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
