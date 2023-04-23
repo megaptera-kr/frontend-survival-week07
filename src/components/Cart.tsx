b78c8d06d0e80062b95fc86f0546a4d86acc4c7d
leejaelll <jayaom91076@gmail.com>
2023-04-23 12:21:43 +0900

    feat: 7주차 과제

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,7 +6,10 @@ export default function Cart() {
   const [snapshot] = useCartStore();
   const { items } = snapshot;
 
-  const totalPrice = items.reduce((acc, food) => acc + (food.price * food.quantity), 0);
+  const totalPrice = items.reduce(
+    (acc, food) => acc + food.price * food.quantity,
+    0
+  );
 
   const handleClickOrder = async () => {
     // eslint-disable-next-line no-alert
@@ -15,40 +18,23 @@ export default function Cart() {
 
   return (
     <div style={{ marginBottom: '3rem' }}>
-      <h2>
-        점심 바구니
-      </h2>
+      <h2>점심 바구니</h2>
       <ul style={{ width: '20%' }}>
         {items.map((item, index) => {
-          const {
-            id, name, price, quantity,
-          } = item;
+          const { id, name, price, quantity } = item;
           const food = { id, name, price };
 
           const key = `${id}-${index}`;
 
           return (
-            <MenuItem
-              key={key}
-              food={food}
-            >
-              <span>
-                *
-                {' '}
-                {quantity}
-              </span>
+            <MenuItem key={key} food={food}>
+              <span>* {quantity}</span>
             </MenuItem>
           );
         })}
       </ul>
-      <button
-        type="button"
-        onClick={handleClickOrder}
-      >
-        합계:
-        {' '}
-        {totalPrice.toLocaleString()}
-        원 주문
+      <button type="button" onClick={handleClickOrder}>
+        합계: {totalPrice.toLocaleString()}원 주문
       </button>
     </div>
   );
---
+++ src/pages/RestaurantDetailPage.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable comma-dangle */
+
+import { useParams } from 'react-router-dom';
+
+import useFetchRestaurants from '../hooks/useFetchRestaurants';
+
+import Restaurants from '../components/Restaurants';
+
+export default function RestaurantDetailPage() {
+  const restauratns = useFetchRestaurants();
+  const { id } = useParams();
+
+  const selectedRestaurant = restauratns.filter(
+    (restaurant) => restaurant.id.toString() === id
+  );
+
+  return <Restaurants restaurants={selectedRestaurant} />;
+}
