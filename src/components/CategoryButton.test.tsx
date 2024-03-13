dd8aaf7a6b7e7863572e18100901f7286be80ad2
swd <tladnjsen@gmail.com>
2024-03-13 13:11:53 +0900

    test: Add component test code
    - CategoryButton
    - CategoryButtons

--- src/components/CategoryButton.test.tsx
+++ src/components/CategoryButton.test.tsx
@@ -1,17 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import CategoryButton from './CategoryButton';
 
-describe('CategoryButton', () => {
-  it('전체', () => {
-    const selectedCategoryName = '전체';
-    render(<CategoryButton selectedCategoryName={selectedCategoryName} />);
-    screen.getByRole('button', { name: '전체' });
-  });
+const context = describe;
 
-  it('한식', () => {
-    const selectedCategoryName = '한식';
-    render(<CategoryButton selectedCategoryName={selectedCategoryName} />);
-    screen.getByRole('button', { name: '한식' });
+describe('CategoryButton', () => {
+  const mockSetCategoryName = jest.fn();
+
+  const rendering = (categoryName: string) => {
+    render(
+      <CategoryButton
+        categoryName={categoryName}
+        setCategoryName={mockSetCategoryName}
+      />,
+    );
+  };
+
+  context('render correclty - category name is 전체', () => {
+    it('전체 버튼이 렌더링 된다.', () => {
+      const categoryName = '전체';
+      rendering(categoryName);
+
+      screen.getByRole('button', { name: categoryName });
+    });
+
+    context('render correclty - category name is 한식', () => {
+      it('한식 버튼이 렌더링 된다.', () => {
+        const categoryName = '한식';
+        rendering(categoryName);
+
+        screen.getByRole('button', { name: categoryName });
+      });
+    });
+
+    context('button click event - category name is 한식', () => {
+      it('한식 버튼이 클릭된다.', () => {
+        const categoryName = '한식';
+        rendering(categoryName);
+
+        const button = screen.getByRole('button', { name: categoryName });
+
+        fireEvent.click(button);
+        expect(mockSetCategoryName).toHaveBeenCalled();
+        expect(mockSetCategoryName).toHaveBeenCalledWith(categoryName);
+        expect(mockSetCategoryName).not.toHaveBeenCalledWith('전체');
+      });
+    });
   });
 });
--- src/components/CategoryButtons.test.tsx
+++ src/components/CategoryButtons.test.tsx
@@ -1,15 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import CategoryButtons from './CategoryButtons';
+
+import useFetchCategories from '../hooks/useFetchCategories';
 
 jest.mock('../hooks/useFetchCategories');
 
 describe('CategoryButtons', () => {
-  it('렌더링', () => {
-    render(<CategoryButtons />);
+  const mockSetCategoryButtonName = jest.fn();
+
+  beforeEach(() => {
+    mockSetCategoryButtonName.mockClear();
+  });
+
+  it('rendering correctly', () => {
+    render(
+      <CategoryButtons setCategoryButtonName={mockSetCategoryButtonName} />,
+    );
     screen.getByRole('button', { name: '전체' });
     screen.getByRole('button', { name: '한식' });
     screen.getByRole('button', { name: '일식' });
     screen.getByRole('button', { name: '중식' });
+  });
+
+  it('button clicked', () => {
+    render(
+      <CategoryButtons setCategoryButtonName={mockSetCategoryButtonName} />,
+    );
+
+    const button전체 = screen.getByRole('button', { name: '전체' });
+    fireEvent.click(button전체);
+    expect(mockSetCategoryButtonName).toHaveBeenCalledWith('전체');
+  });
+
+  it('rendering correctly with only 전체', () => {
+    (useFetchCategories as jest.Mock).mockReturnValueOnce({ categories: [] });
+
+    render(
+      <CategoryButtons setCategoryButtonName={mockSetCategoryButtonName} />,
+    );
+
+    expect(screen.queryByRole('button', { name: '전체' })).toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: '한식' }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: '일식' }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: '중식' }),
+    ).not.toBeInTheDocument();
   });
 });
