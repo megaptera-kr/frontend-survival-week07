1effeefbe391344532b8c6e0d87678b8fdfd72eb
swd <tladnjsen@gmail.com>
2024-03-13 17:28:02 +0900

    chore: Add some fixture

---
+++ fixtures/menu.ts
@@ -0,0 +1,10 @@
+import MenuType from '../src/types/MenuType';
+
+const menu: MenuType[] = [
+  { id: 1, name: '짜장면', price: 8000 },
+  { id: 2, name: '짬뽕', price: 8000 },
+  { id: 3, name: '차돌짬뽕', price: 9000 },
+  { id: 4, name: '탕수육', price: 14000 },
+];
+
+export default menu;
