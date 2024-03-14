39298af868867b6132b2d120f10269414eaf0509
swd <tladnjsen@gmail.com>
2024-03-14 12:46:21 +0900

    feat: Add type CartItemType

---
+++ src/types/CartItemType.ts
@@ -0,0 +1,11 @@
+type CartItemType = {
+  menuId: number;
+  menuName: string;
+  menuPrice: number;
+  restaurantId: number;
+  restaurantName: string;
+  categoryName: string;
+  quantity: number;
+};
+
+export default CartItemType;
