b7a00a92ca1100eaeacfdc16b7268dc962c3dddb
magrowing <kmagrowing@gmail.com>
2024-03-14 18:33:27 +0900

    refactor : [인트로페이지] 매장 주문, 전체 포장 버튼 컴포넌트로 분리

---
+++ src/components/OrderButtons.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from './common/Button';
+
+export default function OrderButtons() {
+  const navigation = useNavigate();
+  const HandelOnClick = () => {
+    navigation('/order');
+  };
+
+  return (
+    <div>
+      <Button text="🙋🏻‍♀️ 매장 주문" onClick={HandelOnClick} />
+      <Button text="🛍️ 전체 포장" onClick={HandelOnClick} />
+    </div>
+  );
+}
--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,20 +1,10 @@
-import { useNavigate } from 'react-router';
-
-import Button from '../components/common/Button';
+import OrderButtons from '../components/OrderButtons';
 
 export default function IntroPage() {
-  const navigation = useNavigate();
-  const HandelOnClick = () => {
-    navigation('/order');
-  };
-
   return (
     <div>
       <h2>원하시는 주문을 터치해주세요</h2>
-      <div>
-        <Button text="🙋🏻‍♀️ 매장 주문" onClick={HandelOnClick} />
-        <Button text="🛍️ 전체 포장" onClick={HandelOnClick} />
-      </div>
+      <OrderButtons />
     </div>
   );
 }
