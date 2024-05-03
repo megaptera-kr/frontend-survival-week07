ed0f2e731203a65fde9aa3fe1f497da4d33dd9cd
WATA-Yiju <yiju.dev@watanow.com>
2024-05-03 17:16:05 +0900

    add: MSW 환경설정 추가

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
+import 'reflect-metadata';
 import 'whatwg-fetch';
 import server from './mocks/server';
 
